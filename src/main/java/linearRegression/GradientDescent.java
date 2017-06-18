15290ec3d6ea0d73841e325f94d0df7eb14f2164
chehuizi <840739100@qq.com>
2017-06-18 17:05:22 +0800

    update gd

--- src/main/java/linearRegression/GradientDescent.java
+++ src/main/java/linearRegression/GradientDescent.java
@@ -1,5 +1,8 @@
 package linearRegression;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class GradientDescent {
 	
 	/**
@@ -8,11 +11,11 @@ public class GradientDescent {
 	 * input: (1, 2), (2, 4), (3, 6), (4, 8), (5, 10)
 	 * 误差: 0.01
 	 */
-	public void calc() {
+	public static void batchGD_one_param() {
 		double m1 = 0, m0 = 0;
 		double[][] input = {{1, 2}, {2, 4}, {3, 6}, {4, 8}, {5, 10}};
 		double deviation = 0.0000001;
-		double step = 0.15;
+		double step = 0.19;
 		
 		double dd = 0;
 		int times = 0;
@@ -42,9 +45,139 @@ public class GradientDescent {
 		} while (dd > deviation);
 	}
 	
-	
-	public static void main(String args[]) {
-		GradientDescent gd = new GradientDescent();
-		gd.calc();
+	/**
+	 * 假设函数：p(x)=p0+p1*x1+p2*x2+p3*x3+···+pn*xn
+	 * 输入：{{x11,x12,x13,···,x1n}, 
+	 * 		 {x21,x22,x23,···,x2n}, 
+	 * 		 {x31,x32,x33,···,x3n}, 
+	 * 		 ··· , 
+	 * 		 {xn1,xn2,xn3,···,xnn}}
+	 * 输出：{y1,y2,y3,···,yn}
+	 * 批量梯度下降算法
+	 * 
+	 * @param paramSize : 参数个数
+	 * @param input : 输入
+	 * @param output : 输出
+	 * @param deviation : 误差
+	 * @param step : 步长
+	 */
+	public static void batchGD(int paramSize, double[][] input, double[] output, double deviation, double step) {
+		double[] params = new double[paramSize];
+		double b = 0;
+		for (int i=0; i<paramSize; i++) {
+			params[i] = 0;
+		}
+		
+		double dd = 0;
+		int times = 0;
+		do {
+			times++;
+			dd = 0;
+			for (int i=0; i<input.length; i++) {
+				double y = output[i];
+				
+				double h = 0;
+				h += 1 * b;
+				for (int j=0; j<input[i].length; j++) {
+					h += params[j] * input[i][j];
+				}
+				dd += (h - y) * (h - y);
+			}
+			dd = 1.0/2.0 * 1/input.length * dd;
+			if (dd > deviation) {
+				double temp = 0;
+				for (int i=0; i<input.length; i++) {
+					double sum = 0;
+					for (int j=0; j<input[i].length; j++) {
+						sum += params[j] * input[i][j];
+					}
+					temp += (b + sum - output[i]) * 1;
+				}
+				b = b - step * 1/input.length * temp;
+				
+				for (int m=0; m<params.length; m++) {
+					temp = 0;
+					for (int i=0; i<input.length; i++) {
+						double sum = 0;
+						for (int j=0; j<input[i].length; j++) {
+							sum += params[j] * input[i][j];
+						}
+						temp += (b + sum - output[i]) * input[i][m];
+					}
+					params[m] = params[m] - step * 1/input.length * temp;
+				}
+			}
+			
+			StringBuffer sb = new StringBuffer();
+			for (int i=0; i<params.length; i++) {
+				sb.append("params[" + i + "]=" + params[i] + ", ");
+			}
+			System.out.println(sb.toString() + "b=" + b + ", dd=" + dd + ", times=" + times);
+		} while (dd > deviation);
 	}
+	
+	/**
+	 * 假设函数：p(x)=p0+p1*x1+p2*x2+p3*x3+···+pn*xn
+	 * 输入：{{x11,x12,x13,···,x1n}, 
+	 * 		 {x21,x22,x23,···,x2n}, 
+	 * 		 {x31,x32,x33,···,x3n}, 
+	 * 		 ··· , 
+	 * 		 {xn1,xn2,xn3,···,xnn}}
+	 * 输出：{y1,y2,y3,···,yn}
+	 * 随机梯度下降算法
+	 * 
+	 * @param paramSize : 参数个数
+	 * @param input : 输入
+	 * @param output : 输出
+	 * @param step : 步长
+	 */
+	public static void stochasticGD(int paramSize, double[][] input, double[] output, double step) {
+		double[] params = new double[paramSize];
+		double b = 0;
+		for (int i=0; i<paramSize; i++) {
+			params[i] = 0;
+		}
+		
+		int times = 0;
+		double dd = 0;
+		
+		for (int i=0; i<input.length; i++) {
+			times++;
+			dd = 0;
+			double y = output[i];
+			double h = 0;
+			h += 1 * b;
+			for (int j=0; j<input[i].length; j++) {
+				h += params[j] * input[i][j];
+			}
+			dd += (h - y) * (h - y);
+			dd = 1.0/2.0 * 1/input.length * dd;
+			
+			double temp = 0;
+			double sum = 0;
+			for (int j=0; j<input[i].length; j++) {
+				sum += params[j] * input[i][j];
+			}
+			temp += (b + sum - output[i]) * 1;
+			
+			b = b - step * 1/input.length * temp;
+			
+			for (int m=0; m<params.length; m++) {
+				temp = 0;
+				sum = 0;
+				for (int j=0; j<input[i].length; j++) {
+					sum += params[j] * input[i][j];
+				}
+				temp += (b + sum - output[i]) * input[i][m];
+				params[m] = params[m] - step * 1/input.length * temp;
+			}
+			
+			StringBuffer sb = new StringBuffer();
+			for (int j=0; j<params.length; j++) {
+				sb.append("params[" + j + "]=" + params[j] + ", ");
+			}
+			System.out.println(sb.toString() + "b=" + b + ", dd=" + dd + ", times=" + times);
+		}
+	}
+	
 }
---
+++ src/main/test/linearRegression/GradientDescentTest.java
@@ -0,0 +1,34 @@
+package linearRegression;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GradientDescentTest {
+
+	@Test
+	public void testBatchGD() {
+		int paramSize =1;
+		double[][] input = {{1}, {2}, {3}, {4}, {5}};
+		double[] output = {2, 4, 6, 8, 10}; 
+		double deviation = 0.0000001;
+		double step = 0.18;
+		
+		GradientDescent.batchGD(paramSize, input, output, deviation, step);
+	}
+	
+	@Test
+	public void testStochasticGD() {
+		int paramSize =1;
+		int size = 30;
+		double[][] input = new double[size][];
+		double[] output = new double[size];
+		for (int i=0; i<size; i++) {
+			input[i] = new double[]{i+1};
+			output[i] = (i+1) * 2;
+		}
+		double step = 0.18;
+		
+		GradientDescent.stochasticGD(paramSize, input, output, step);
+	}
+}
