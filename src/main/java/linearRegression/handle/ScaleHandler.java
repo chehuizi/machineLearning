809ac5adb4d61f0d168b1574b0fb1a22dae43b6d
chehuizi <840739100@qq.com>
2017-07-15 11:00:48 +0800

    add

---
+++ src/main/java/linearRegression/handle/ScaleHandler.java
@@ -0,0 +1,10 @@
+package linearRegression.handle;
+
+public interface ScaleHandler {
+	
+	/**
+	 * 数据
+	 */
+	public void scale();
+	
+}
---
+++ src/main/java/linearRegression/handle/VerifyHandler.java
@@ -0,0 +1,9 @@
+package linearRegression.handle;
+
+public interface VerifyHandler {
+	/**
+	 * 验证
+	 */
+	public void verify();
+	
+}
