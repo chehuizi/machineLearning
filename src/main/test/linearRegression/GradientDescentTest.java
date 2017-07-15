e77cca543d166af73cf5ffe444919a06e08a2037
chehuizi <840739100@qq.com>
2017-07-15 11:16:07 +0800

    update

--- src/main/test/linearRegression/GradientDescentTest.java
+++ src/main/test/linearRegression/GradientDescentTest.java
@@ -6,27 +6,38 @@ public class GradientDescentTest {
 
 	@Test
 	public void testBatchGD() {
-		int paramSize =1;
 		double[][] input = {{1}, {2}, {3}, {4}, {5}};
 		double[] output = {2, 4, 6, 8, 10}; 
 		double deviation = 0.0000001;
 		double step = 0.18;
 		
-		GradientDescent.batchGD(paramSize, input, output, deviation, step);
+		GradientDescent gd = new GradientDescent();
+		gd.getLinearModel().setInput(input);
+		gd.getLinearModel().setOutput(output);
+		gd.getLinearModel().setDeviation(deviation);
+		gd.getLinearModel().setStep(step);
+		
+		gd.batchGD();
 	}
 	
 	@Test
 	public void testStochasticGD() {
-		int paramSize =1;
 		int size = 30;
 		double[][] input = new double[size][];
 		double[] output = new double[size];
+		double deviation = 0.0000001;
 		for (int i=0; i<size; i++) {
 			input[i] = new double[]{i+1};
 			output[i] = (i+1) * 2;
 		}
 		double step = 0.18;
 		
-		GradientDescent.stochasticGD(paramSize, input, output, step);
+		GradientDescent gd = new GradientDescent();
+		gd.getLinearModel().setInput(input);
+		gd.getLinearModel().setOutput(output);
+		gd.getLinearModel().setDeviation(deviation);
+		gd.getLinearModel().setStep(step);
+		
+		gd.stochasticGD(1, input, output, step);
 	}
 }
